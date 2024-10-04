e0ddf6a730ad107e575c70777d4523702312d559
jeromee <119032315+akiii22@users.noreply.github.com>
2024-10-04 19:22:26 +0800

    Complete updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,8 @@ import Skills from "./components/Skills";
 
 function App() {
   return (
-<<<<<<< HEAD
+
     <>
-=======
-    <div className="font-sans overflow-x-hidden">
->>>>>>> 9922a99bbeabdbdc45e316e617dee43354ebadd8
       <Navabar />
       <div className="overflow-x-hidden font-sans">
         <Home />
